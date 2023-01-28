95f03bb65d8c07575df09e611d3cd368fb5acdcf
Pedro Klein <pedroklein1@hotmail.com>
2023-01-28 13:38:41 -0300

    add: Add database tables creation

--- schema.sql
+++ schema.sql
@@ -25,7 +25,7 @@ CREATE TABLE price (
                                     ON DELETE CASCADE,
     product_id INT PRIMARY KEY REFERENCES product(product_id)
                                     ON DELETE CASCADE,
-    price numeric(5,2),
+    price numeric(5,2) NOT NULL CHECK (price >= 0),
     discount DISCOUNT
 );
 
@@ -37,36 +37,50 @@ CREATE TABLE region (
 );
 
 CREATE TYPE REFUND_TYPE AS ENUM ('REFUNDABLE', 'SELF_REFUNDABLE', 'NON_REFUNDABLE');
+CREATE TYPE PRODUCT_TYPE AS ENUM ('GAME', 'ADDON', 'DEMO', 'EDITION', 'BUNDLE');
 
 CREATE TABLE product (
     product_id SERIAL PRIMARY KEY,
-    release_date DATE,
+    release_date DATE NOT NULL,
     initial_release_date DATE,
-    cover_img VARCHAR(200),
-    refund_type REFUND_TYPE,
-    description VARCHAR(2000),
-    title VARCHAR(100)
+    cover_img VARCHAR(200) NOT NULL,
+    refund_type REFUND_TYPE NOT NULL,
+    description VARCHAR(2000) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    type PRODUCT_TYPE NOT NULL
 );
 
 CREATE TYPE MEDIA_TYPE AS ENUM ('IMAGE', 'VIDEO');
 
 CREATE TABLE media (
-    order INT PRIMARY KEY,
+    order INT PRIMARY KEY check (order >= 0),
     product_id INT PRIMARY KEY REFERENCES product(product_id)
                                     ON DELETE CASCADE,
-    url VARCHAR(200),
-    media_type MEDIA_TYPE,
+    url VARCHAR(200) NOT NULL,
+    media_type MEDIA_TYPE NOT NULL,
+);
+
+CREATE TABLE bundle (
+    product_id INT PRIMARY KEY REFERENCES product(product_id)
+                                    ON DELETE CASCADE
+);
+
+CREATE TABLE package (
+    product_id INT REFERENCES product(product_id)
+                        ON DELETE CASCADE,
+    bundle_id INT REFERENCES bundle(product_id)
+                        ON DELETE CASCADE
 );
 
 CREATE TABLE game (
     product_id INT PRIMARY KEY REFERENCES product(product_id)
                                     ON DELETE CASCADE,
     game_id SERIAL PRIMARY KEY,
-    developer VARCHAR(200),
-    publisher VARCHAR(200),
-    social_newtwork VARCHAR(200) ARRAY[]
+    developer VARCHAR(200) NOT NULL,
+    publisher VARCHAR(200) NOT NULL,
+    social_newtworks VARCHAR(200) ARRAY[]
     laucher_name VARCHAR(100),
-    language VARCHAR(50) ARRAY[]
+    language VARCHAR(50) ARRAY[] NOT NULL CHECK (array_length(language, int) > 0)
 );
 
 CREATE TABLE genre (
@@ -99,80 +113,47 @@ CREATE TABLE achievement (
     name VARCHAR(200) PRIMARY KEY,
     game_id INT PRIMARY KEY REFERENCES game(game_id)
                                 ON DELETE CASCADE,
-    description VARCHAR(200),
-    xp INT,
-    category ACHIEVEMENT_CATEGORY
+    description VARCHAR(200) NOT NULL,
+    xp INT NOT NULL CHECK (xp >= 0 AND xp <= 1000),
+    category ACHIEVEMENT_CATEGORY NOT NULL
 );
 
-CREATE TYPE CONTENT_TYPE AS ENUM ('ADDON', 'DEMO', 'EDITION');
-CREATE TYPE ADDON_TYPE AS ENUM ('BOOK', 'GAME_ADDON', 'SOUNDTRACK', 'VIDEO');
+-- CREATE TYPE CONTENT_TYPE AS ENUM ('ADDON', 'DEMO', 'EDITION');
 
-CREATE TABLE content (
-    content_id SERIAL PRIMARY KEY,
+-- CREATE TABLE content (
+--     product_id INT PRIMARY KEY REFERENCES product(product_id)
+--                                     ON DELETE CASCADE,
+--     content_type CONTENT_TYPE NOT NULL,
+--     duration INTERVAL,
+--     addon_type ADDON_TYPE,
+--     game_id INT NOT NULL REFERENCES game(game_id)
+-- );
+
+CREATE TYPE ADDON_TYPE AS ENUM ('BOOK', 'GAME_ADDON', 'SOUNDTRACK', 'VIDEO');
+CREATE TABLE addon (
     product_id INT PRIMARY KEY REFERENCES product(product_id)
                                     ON DELETE CASCADE,
-    content_type CONTENT_TYPE,
-    duration INTERVAL,
-    addon_type ADDON_TYPE,
+    addon_type ADDON_TYPE NOT NULL,
     game_id INT NOT NULL REFERENCES game(game_id)
-                            ON DELETE CASCADE
+);
+
+CREATE TABLE demo (
+    product_id INT PRIMARY KEY REFERENCES product(product_id)
+                                    ON DELETE CASCADE,
+    duration INTERVAL,
+    game_id INT NOT NULL REFERENCES game(game_id)
+);
+
+CREATE TABLE edition (
+    product_id INT PRIMARY KEY REFERENCES product(product_id)
+                                    ON DELETE CASCADE,
+    game_id INT NOT NULL REFERENCES game(game_id)
 );
 
 CREATE TABLE requirement (
     os VARCHAR(50) PRIMARY KEY,
     game_id INT PRIMARY KEY REFERENCES game(game_id)
                                 ON DELETE CASCADE,
-    minimum JSON,
-    recommended JSON
+    minimum JSON NOT NULL,
+    recommended JSON NOT NULL
 );
-
---TODO: check
-CREATE TABLE bundle (
-);
-
-CREATE TABLE package (
-    product_id INT REFERENCES product(product_id)
-                        ON DELETE CASCADE
-);
- 
-ALTER TABLE Price ADD CONSTRAINT FK_Price_1
-    FOREIGN KEY (fk_Region_region_id)
-    REFERENCES Region (region_id)
-    ON DELETE SET NULL;
- 
-ALTER TABLE Price ADD CONSTRAINT FK_Price_2
-    FOREIGN KEY (fk_Product_product_id)
-    REFERENCES Product (product_id)
-    ON DELETE SET NULL;
- 
-ALTER TABLE Category ADD CONSTRAINT FK_Category_1
-    FOREIGN KEY (fk_Genre_genre_id)
-    REFERENCES Genre (genre_id)
-    ON DELETE RESTRICT;
- 
-ALTER TABLE Category ADD CONSTRAINT FK_Category_2
-    FOREIGN KEY (fk_Game_fk_Product_product_id)
-    REFERENCES ??? (???);
- 
-ALTER TABLE Classification ADD CONSTRAINT FK_Classification_1
-    FOREIGN KEY (fk_Feature_feature_id)
-    REFERENCES Feature (feature_id)
-    ON DELETE RESTRICT;
- 
-ALTER TABLE Classification ADD CONSTRAINT FK_Classification_2
-    FOREIGN KEY (fk_Game_fk_Product_product_id)
-    REFERENCES ??? (???);
- 
-ALTER TABLE Review ADD CONSTRAINT FK_Review_1
-    FOREIGN KEY (fk_User_user_id, fk_User_email, fk_User_nickname)
-    REFERENCES User (user_id, email, nickname)
-    ON DELETE SET NULL;
- 
-ALTER TABLE Review ADD CONSTRAINT FK_Review_2
-    FOREIGN KEY (fk_Game_fk_Product_product_id)
-    REFERENCES ??? (???);
- 
-ALTER TABLE Package ADD CONSTRAINT FK_Package_1
-    FOREIGN KEY (fk_Product_product_id)
-    REFERENCES Product (product_id)
-    ON DELETE RESTRICT;
