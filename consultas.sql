07cc16aeb0796c81485ca0c0af9593fa5b759997
enrico <enrico.pizzol@gmail.com>
2023-03-26 19:14:46 -0300

    add queries

--- consultas.sql
+++ consultas.sql
@@ -19,4 +19,57 @@ SELECT title, cover_img, favourite
 FROM users NATURAL JOIN library NATURAL JOIN games NATURAL JOIN products
 WHERE user_id = 1;
 
+-- List the total price to buy all products listed in the catalog
+SELECT sum(price), title 
+FROM products NATURAL JOIN prices;
 
+
+-- consultas com groupby
+
+-- List the number of products of which type listed in the catalog 
+SELECT COUNT(products), type 
+FROM products 
+GROUP BY type;
+
+-- List users with more than one game review
+SELECT nickname, count(reviews)
+FROM users NATURAL JOIN reviews
+GROUP BY user_id
+HAVING count(reviews) > 1;
+
+-- List titles that are listed as more than one type of product
+SELECT COUNT(product_id), title 
+FROM products 
+GROUP BY title
+HAVING COUNT(product_id) > 1;
+
+
+-- List titles that are listed as more than one type of product
+SELECT sum(price), title 
+FROM products NATURAL JOIN prices
+GROUP BY product_id
+HAVING COUNT(product_id) > 1;
+
+
+-- List user that have at least one game in their librarys
+SELECT nickname, count(game_id) 
+FROM users NATURAL JOIN library
+GROUP BY game_id
+HAVING COUNT(game_id) > 1;
+
+-- queries com subqueries
+
+-- List products that a user have that are more expensive than the average price of a game
+SELECT nickname, title, price 
+FROM users NATURAL JOIN library NATURAL JOIN games NATURAL JOIN products NATURAL JOIN prices
+WHERE price > (SELECT avg(price) FROM prices);
+
+-- List products that a user have that are more expensive than the average price of a game
+SELECT nickname, title, price 
+FROM users NATURAL JOIN library NATURAL JOIN games NATURAL JOIN products NATURAL JOIN prices
+WHERE price > (SELECT avg(price) FROM prices);
+
+
+
+
+SELECT 
