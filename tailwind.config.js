c838846d5f9a99dc6728ea9eac834923f6c37e74
Pedro Klein <pedroklein1@hotmail.com>
2023-04-03 22:00:58 -0300

    config: tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+const generateColorClass = (variable) => {
+  return ({ opacityValue }) =>
+    opacityValue
+      ? `rgba(var(--${variable}), ${opacityValue})`
+      : `rgb(var(--${variable}))`;
+};
+
+const textColor = {
+  default: generateColorClass("text-default"),
+  neutral: generateColorClass("text-neutral"),
+};
+
+const backgroundColor = {
+  default: generateColorClass("bg-default"),
+  neutral: generateColorClass("bg-neutral"),
+};
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      textColor,
+      backgroundColor,
+      colors: {
+        primary: generateColorClass("primary-color"),
+        secondary: generateColorClass("secondary-color"),
+        tertiary: generateColorClass("tertiary-color"),
+      },
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+      },
+    },
   },
+  darkMode: ["class", '[data-theme="dark"]'],
   plugins: [],
 };
